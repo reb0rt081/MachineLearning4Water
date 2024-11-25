44f01a472afd27e062caa12643262c393c53babe
Roberto Ribes <rbo@tgw-group.com>
2024-11-25 11:57:28 +0100

    Test for matrix mult

--- tests/Python.U.Test/PythonTests.cs
+++ tests/Python.U.Test/PythonTests.cs
@@ -39,5 +39,34 @@ namespace MachineLearning4Water.Python.U.Test
             Assert.AreEqual(10, solution[1][0]);
             Assert.AreEqual(12, solution[1][1]);
         }
+
+        [TestMethod]
+        public void ExecutePythonScriptMatrixMult()
+        {
+            // Define the matrices
+            // | 1 2 |
+            // | 3 4 |
+            var matrix1 = new List<List<double>> {
+                new List<double> { 1, 2 },
+                new List<double> { 3, 4 }
+            };
+
+            // | 5 6 |
+            // | 7 8 |
+            var matrix2 = new List<List<double>> {
+                new List<double> { 5, 6 },
+                new List<double> { 7, 8 }
+            };
+            List<List<double>> solution = new List<List<double>>();
+            PythonRunner.RunPythonMethod("BaseLib.py", "MatrixMult", result => solution = PythonHelper.ConvertDynamicToMatrix(result), matrix1, matrix2);
+
+            // Solution:
+            // | 6  8  |
+            // | 10 12 |
+            Assert.AreEqual(19, solution[0][0]);
+            Assert.AreEqual(22, solution[0][1]);
+            Assert.AreEqual(43, solution[1][0]);
+            Assert.AreEqual(50, solution[1][1]);
+        }
     }
 }
