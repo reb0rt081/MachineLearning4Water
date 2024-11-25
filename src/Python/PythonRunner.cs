77ad058090321321d41ba2f76fcc871e621041a0
Roberto Ribes <rbo@tgw-group.com>
2024-11-25 10:39:03 +0100

    New test and conversion to matrix

--- src/Python/PythonRunner.cs
+++ src/Python/PythonRunner.cs
@@ -6,7 +6,7 @@ namespace MachineLearning4Water.Python
 {
     public class PythonRunner
     {
-        public static TOut RunPythonMethod<TOut>(string pythonScriptName, string methodName, Func<string, TOut> dataConversionResult, params object[] args)
+        public static TOut RunPythonMethod<TOut>(string pythonScriptName, string methodName, Func<dynamic, TOut> dataConversionResult, params object[] args)
         {
             return PythonHelper.ExecutePythonMethod($"./Scripts/{pythonScriptName}", methodName, dataConversionResult, args);
         }
