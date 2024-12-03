9b5b55b5740377fe40d2809c24baa75f194822af
Roberto Ribes <rbo@tgw-group.com>
2024-12-03 08:32:50 +0100

    Refactoring code

--- tests/Python.U.Test/PythonTests.cs
+++ tests/Python.U.Test/PythonTests.cs
@@ -75,56 +75,59 @@ namespace MachineLearning4Water.Python.U.Test
 
         [TestMethod]
         public void ExecutePythonScriptMatrixDeterminant()
-{
-    // Define the 2x2 matrix
-    var matrix2x2 = new List<List<double>> {
-        new List<double> { 1, 2 },
-        new List<double> { 3, 4 }
-    };
+        {
+            // Define the 2x2 matrix
+            var matrixRange2 = new List<List<double>> 
+            {
+                new List<double> { 1, 2 },
+                new List<double> { 3, 4 }
+            };
 
-    // Calculate determinant for 2x2 matrix
-    int solution2x2 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant", 
-        dyn => int.Parse(dyn.ToString()), matrix2x2);
+            // Calculate determinant for 2x2 matrix
+            int solutionRange2 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant",
+                dyn => int.Parse(dyn.ToString()), matrixRange2);
 
-    // Validate the result
-    Assert.AreEqual(-2, solution2x2);
+            // Validate the result
+            Assert.AreEqual(-2, solutionRange2);
 
-    // Define the 3x3 matrix
-    var matrix3x3 = new List<List<double>> {
-        new List<double> { 6, 1, 1 },
-        new List<double> { 4, -2, 5 },
-        new List<double> { 2, 8, 7 }
-    };
+            // Define the 3x3 matrix
+            var matrixRange3 = new List<List<double>> 
+            {
+                new List<double> { 6, 1, 1 },
+                new List<double> { 4, -2, 5 },
+                new List<double> { 2, 8, 7 }
+            };
 
-    // Calculate determinant for 3x3 matrix
-    int solution3x3 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant", 
-        dyn => int.Parse(dyn.ToString()), matrix3x3);
+            // Calculate determinant for 3x3 matrix
+            int solutionRange3 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant",
+                dyn => int.Parse(dyn.ToString()), matrixRange3);
 
-    // Validate the result
-    Assert.AreEqual(-306, solution3x3);
+            // Validate the result
+            Assert.AreEqual(-306, solutionRange3);
 
-    // Define the pre-determined 10x10 matrix
-    var matrix10x10 = new List<List<double>> {
-        new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
-        new List<double> { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
-        new List<double> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 },
-        new List<double> { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 },
-        new List<double> { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
-        new List<double> { 12, 11, 10, 9, 8, 7, 6, 5, 4, 3 },
-        new List<double> { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 },
-        new List<double> { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4 },
-        new List<double> { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 },
-        new List<double> { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5 }
-    };
+            // Define the pre-determined 10x10 matrix
+            var matrixRange10 = new List<List<double>> 
+            {
+                new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                new List<double> { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+                new List<double> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 },
+                new List<double> { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 },
+                new List<double> { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+                new List<double> { 12, 11, 10, 9, 8, 7, 6, 5, 4, 3 },
+                new List<double> { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 },
+                new List<double> { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4 },
+                new List<double> { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 },
+                new List<double> { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5 }
+            };
 
-    // Calculate determinant for 10x10 matrix
-    int solution10x10 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant",
-        dyn => int.Parse(dyn.ToString()), matrix10x10);
+            // Calculate determinant for 10x10 matrix
+            int solutionRange10 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant",
+                dyn => int.Parse(dyn.ToString()), matrixRange10);
 
-    // Validate the result (determinant is 0)
-    Assert.AreEqual(0, solution10x10);
-}
-     
+            // Validate the result (determinant is 0)
+            Assert.AreEqual(0, solutionRange10);
+        }
+
         //  TODO add a test for each new method: included the ones in MachineLearningTechni
     }
 }
