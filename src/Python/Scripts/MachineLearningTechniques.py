24ba8b10685ef9d90705d876149246c4345600fd
Alex-F-S <alexandre.font.saprykin@estudiantat.upc.edu>
2024-12-03 11:18:48 +0100

    Update MachineLearningTechniques.py
    Added k-means model and parameters functions

--- src/Python/Scripts/MachineLearningTechniques.py
+++ src/Python/Scripts/MachineLearningTechniques.py
@@ -1,6 +1,8 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
+from sklearn.cluster import KMeans
+
 
 
 def LinearModel(x, y):
@@ -23,4 +25,36 @@ def LinearModelParams(x,y):
   slope = model.coef_
   intercept = model.intercept_
   return slope, intercept
-  
+
+def KMeansModel(clusters, x, **kwargs):
+    """
+    Returns a fitted KMeans model.
+    
+    Parameters:
+    clusters (int): Number of clusters.
+    x (array-like): Input data to cluster.
+    **kwargs: Additional keyword arguments for sklearn's KMeans.
+    
+    Returns:
+    KMeans: Fitted KMeans model.
+    """
+    model = KMeans(n_clusters=clusters, **kwargs)
+    model.fit(x)
+    return model
+
+
+def KMeansParams(clusters, x, **kwargs):
+    """
+    Fits a KMeans model and returns cluster centers, labels, and inertia.
+    
+    Parameters:
+    clusters (int): Number of clusters.
+    x (array-like): Input data to cluster.
+    **kwargs: Additional keyword arguments for sklearn's KMeans.
+    
+    Returns:
+    tuple: (cluster_centers, labels, inertia)
+    """
+    model = KMeans(n_clusters=clusters, **kwargs)
+    model.fit(x)
+    return model.cluster_centers_, model.labels_, model.inertia_
