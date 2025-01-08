f09e60ef904fbc008f23b1b275645c102bf29e54
Alex-F-S <alexandre.font.saprykin@estudiantat.upc.edu>
2025-01-08 07:52:49 +0100

    Create MachineLearningTechnices.py (#4)
    * Create MachineLearningTechnices.py
    
    Add imports for sklearn, numpy and pandas, as well as two functions to return a sklearn linear model, one with np input and the other with pd input
    
    * Update BaseLib.py
    
    Añadir determinante, adjunta y inversa de una matriz nativa
    
    * Update BaseLib.py
    
    * Fixing some comments
    
    * Update MachineLearningTechniques.py
    
    Simplified model return function and added a function to return slope and intercept from linear model
    
    * Update BaseLib.py
    
    Updated namings for functions
    
    * Update PythonTests.cs
    
    Matrix unit test
    
    * Refactoring code
    
    * Fixing method
    
    * Unit test for matrix inverse
    
    * Update BaseLib.py
    
    Changed determinant to use LU algorithm
    
    * Update MachineLearningTechniques.py
    
    Added k-means model and parameters functions
    
    * Update MachineLearningTechniques.py
    
    Added logistic regression model return and parameters return
    
    * Update MachineLearningTechniques.py
    
    Added SVC Model, Logistic Model, KMeans model, KNN model and evaluation function for any type of model
    
    * Update PythonTests.cs
    
    Added unit test for LinearModelParams from MachineLearningTechniques.py
    
    * Setting  option to script
    
    * Removing unnecessary test
    
    * Update python-build-test.yml
    
    Update lib imports
    
    * Update test_BaseLib.py
    
    Added tests for DotProd, MatrixSum, MatrixMult and Determinant
    
    * Update python-build-test.yml
    
    Corrected scikit-learn import
    
    * Update test_BaseLib.py
    
    Re-test
    
    * Update test_BaseLib.py
    
    Added test for transpose and inverse
    
    * Update test_BaseLib.py
    
    Fixed error in assert
    
    * Create test_MachineLearningTechniques.py
    
    Added test file for MachineLearningTechniques with tests for all models
    
    * Delete src/Python/test_MachineLearningTechniques.py
    
    Deleted and moved to scripts foler
    
    * Create test_MachineLearningTechniques.py
    
    Added unit test for models in MachineLearningTechniques
    
    * Update test_MachineLearningTechniques.py
    
    Fixed X array dimensions
    
    * Update test_MachineLearningTechniques.py
    
    Revised asserts
    
    * Update test_MachineLearningTechniques.py
    
    Rev 2 for linear model and KNN
    
    * Update test_MachineLearningTechniques.py
    
    Changed to np array in linear model due to shape attribute missing
    
    * Update test_MachineLearningTechniques.py
    
    Added missing np import
    
    * Update test_MachineLearningTechniques.py
    
    Fixed error with numpy conversion
    
    * Update test_MachineLearningTechniques.py
    
    Fixed linear model intercept access
    
    * Update MachineLearningTechniques.py
    
    Added Ridge Model
    
    * Update MachineLearningTechniques.py
    
    Fixed comment
    
    * Update test_MachineLearningTechniques.py
    
    Fixed test for kmeans
    
    * Update test_MachineLearningTechniques.py
    
    Fixed typo with inertia
    
    * Update MachineLearningTechniques.py
    
    Fixed flattening on kmeansparams return of centroids
    
    * Update test_MachineLearningTechniques.py
    
    Serms that the kmeansparams function returns the centroids in reverse order
    
    * Update test_MachineLearningTechniques.py
    
    Fixed the classification test in KMeans Params
    
    * Update test_MachineLearningTechniques.py
    
    It seems sklearn doesnt mind the order of the centroids, so they are now ordered in ascending order and classifications cannot be tested, but under revision of the test outputs, they are correct
    
    * Update MachineLearningTechniques.py
    
    Update test
    
    ---------
    
    Co-authored-by: Roberto Ribes <rbo@tgw-group.com>

---
+++ src/Python/Scripts/MachineLearningTechniques.py
@@ -0,0 +1,161 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, mean_squared_error, mean_absolute_error, r2_score
+from sklearn.base import is_classifier
+from sklearn.linear_model import Ridge
+
+#Models
+def LinearModel(x, y):
+  """returns fit linear model from sklearn"""
+  
+  
+  if x.shape[0] != y.shape[0]:
+    raise ValueError("The number of samples in X and y must be the same.")  
+  model = LinearRegression()
+  model.fit(x, y)
+  return model
+
+def LinearModelParams(x,y):
+
+  """returns slope and intercept for linear estimator"""
+  if x.shape[0] != y.shape[0]:
+    raise ValueError("The number of samples in X and y must be the same.")  
+  model = LinearRegression()
+  model.fit(x, y)
+  slope = model.coef_
+  intercept = model.intercept_
+  return slope, intercept
+
+def KMeansModel(clusters, x, **kwargs):
+    """
+    Returns a fitted KMeans model.
+    
+    Parameters:
+    clusters (int): Number of clusters.
+    x (array-like): Input data to cluster.
+    **kwargs: Additional keyword arguments for sklearn's KMeans.
+    
+    Returns:
+    KMeans: Fitted KMeans model.
+    """
+    model = KMeans(n_clusters=clusters, **kwargs)
+    model.fit(x)
+    return model
+
+
+def KMeansParams(clusters, x, **kwargs):
+    """
+    Fits a KMeans model and returns cluster centers, labels, and inertia.
+    
+    Parameters:
+    clusters (int): Number of clusters.
+    x (array-like): Input data to cluster.
+    **kwargs: Additional keyword arguments for sklearn's KMeans.
+    
+    Returns:
+    tuple: (cluster_centers, labels, inertia)
+    """
+    model = KMeans(n_clusters=clusters, **kwargs)
+    model.fit(x)
+    return model.cluster_centers_.flatten(), model.labels_, model.inertia_
+
+def LogisticRegressionModel(x, y, **kwargs):
+    """
+    Fits a Logistic Regression model and returns the model itself.
+    
+    Parameters:
+    x (array-like): Feature matrix.
+    y (array-like): Target values.
+    **kwargs: Additional keyword arguments for sklearn's LogisticRegression.
+    
+    Returns:
+    LogisticRegression: The fitted Logistic Regression model.
+    """
+    model = LogisticRegression(**kwargs)
+    model.fit(x, y)
+    return model
+
+
+def LogisticRegressionParams(x, y, **kwargs):
+    """
+    Fits a Logistic Regression model and returns coefficients and intercept.
+    
+    Parameters:
+    x (array-like): Feature matrix.
+    y (array-like): Target values.
+    **kwargs: Additional keyword arguments for sklearn's LogisticRegression.
+    
+    Returns:
+    tuple: (coefficients, intercept)
+    """
+    model = LogisticRegression(**kwargs)
+    model.fit(x, y)
+    return model.coef_, model.intercept_
+
+
+def SVCModel (x,y,**kwargs):
+  """Takes x and y as array-like inputs and returns a SVC classifier"""
+
+  model = SVC(**kwargs)
+  model.fit(x,y)
+  
+  return model
+
+
+
+def KNNModel (x,y,**kwargs):
+  """Takes x and y as array-like inputs and outputs a KNN classifier"""
+
+  model=KNeighborsClassifier(**kwargs)
+  model.fit(x,y)
+  return model
+
+#Model Eval
+
+def RidgeRegressionModel (x,y, alpha, **kwargs):
+  """takes x and y as array like object and outputs a RR model"""
+
+  model = Ridge(alpha = alpha, **kwargs)
+  model.fit(x,y)
+  return model
+  #
+  
+def evaluate_basic_metrics(model, X_test, y_test):
+    """
+    Evaluates basic metrics of a given model (classifier or regressor).
+    
+    Parameters:
+    - model: Trained model (classifier or regressor)
+    - X_test: Test data (features)
+    - y_test: True labels/values (target)
+    
+    Returns:
+    - Dictionary of basic evaluation metrics
+    """
+    metrics = {}
+
+    # Check if the model is a classifier
+    if is_classifier(model):
+        # Predictions for classification models
+        y_pred = model.predict(X_test)
+        
+        # Basic classification metrics
+        metrics['accuracy'] = accuracy_score(y_test, y_pred)
+        metrics['precision'] = precision_score(y_test, y_pred, average='macro', zero_division=0)
+        metrics['confusion_matrix'] = confusion_matrix(y_test, y_pred)
+    
+    else:
+        # Predictions for regression models
+        y_pred = model.predict(X_test)
+        
+        # Basic regression metrics
+        metrics['mean_squared_error'] = mean_squared_error(y_test, y_pred)
+        metrics['mean_absolute_error'] = mean_absolute_error(y_test, y_pred)
+        metrics['r2_score'] = r2_score(y_test, y_pred)
+    
+    return metrics
--- tests/Python.U.Test/PythonTests.cs
+++ tests/Python.U.Test/PythonTests.cs
@@ -72,5 +72,91 @@ namespace MachineLearning4Water.Python.U.Test
             Assert.AreEqual(43, solution[1][0]);
             Assert.AreEqual(50, solution[1][1]);
         }
+
+        [TestMethod]
+        public void ExecutePythonScriptMatrixDeterminant()
+        {
+            // Define the 2x2 matrix
+            var matrixRange2 = new List<List<double>> 
+            {
+                new List<double> { 1, 2 },
+                new List<double> { 3, 4 }
+            };
+
+            // Calculate determinant for 2x2 matrix
+            int solutionRange2 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant",
+                dyn => int.Parse(dyn.ToString()), matrixRange2);
+
+            // Validate the result
+            Assert.AreEqual(-2, solutionRange2);
+
+            // Define the 3x3 matrix
+            var matrixRange3 = new List<List<double>> 
+            {
+                new List<double> { 6, 1, 1 },
+                new List<double> { 4, -2, 5 },
+                new List<double> { 2, 8, 7 }
+            };
+
+            // Calculate determinant for 3x3 matrix
+            int solutionRange3 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant",
+                dyn => int.Parse(dyn.ToString()), matrixRange3);
+
+            // Validate the result
+            Assert.AreEqual(-306, solutionRange3);
+
+            // Define the pre-determined 10x10 matrix
+            var matrixRange10 = new List<List<double>> 
+            {
+                new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                new List<double> { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+                new List<double> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 },
+                new List<double> { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 },
+                new List<double> { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+                new List<double> { 12, 11, 10, 9, 8, 7, 6, 5, 4, 3 },
+                new List<double> { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 },
+                new List<double> { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4 },
+                new List<double> { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 },
+                new List<double> { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5 }
+            };
+
+            // Calculate determinant for 10x10 matrix
+            int solutionRange10 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixDeterminant",
+                dyn => int.Parse(dyn.ToString()), matrixRange10);
+
+            // Validate the result (determinant is 0)
+            Assert.AreEqual(0, solutionRange10);
+        }
+
+        [TestMethod]
+        public void ExecutePythonScriptMatrixInverse()
+        {
+            // Define the 3x3 matrix
+            var matrixRange3 = new List<List<double>>
+            {
+                new List<double> { 2, 1, 1 },
+                new List<double> { 1, 2, 1 },
+                new List<double> { 1, 1, 2 }
+            };
+
+            // Calculate determinant for 3x3 matrix
+            var solutionRange3 = PythonRunner.RunPythonMethod("BaseLib.py", "MatrixInverse",
+                result => PythonHelper.ConvertDynamicToMatrix(result), matrixRange3);
+
+            // Solution:
+            // | 0.75 -0.25 -0.25 |
+            // | -0.25 0.75 -0.25 |
+            // | -0.25 -0.25 0.75 |
+            Assert.AreEqual(0.75, solutionRange3[0][0]);
+            Assert.AreEqual(-0.25, solutionRange3[0][1]);
+            Assert.AreEqual(-0.25, solutionRange3[0][2]);
+            Assert.AreEqual(-0.25, solutionRange3[1][0]);
+            Assert.AreEqual(0.75, solutionRange3[1][1]);
+            Assert.AreEqual(-0.25, solutionRange3[1][2]);
+            Assert.AreEqual(-0.25, solutionRange3[2][0]);
+            Assert.AreEqual(-0.25, solutionRange3[2][1]);
+            Assert.AreEqual(0.75, solutionRange3[2][2]);
+
+        }
     }
 }
