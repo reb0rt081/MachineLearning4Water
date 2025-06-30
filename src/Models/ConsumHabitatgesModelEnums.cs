082987796c0deee81d324374f7174a51e412bf66
Roberto Ribes <rbo@tgw-group.com>
2025-06-30 08:44:02 +0200

    Enum class

---
+++ src/Models/ConsumHabitatgesModelEnums.cs
@@ -0,0 +1,51 @@
+﻿namespace MachineLearning4Water.Models
+{
+    public sealed class ConsumHabitatgesModelEnums
+    {
+        public enum TipusHabitatghe
+        {
+            VivendaUnifamiliar,
+            Pis
+        }
+
+        public enum Banyera
+        {
+            NoTincBanyera,
+            TincBanyeraMaiUtilitzo,
+            TincBanyeraUtilitzoPuntualment,
+            TincBanyeraUtilitzoSovint
+        }
+
+        public enum TipusCaldera
+        {
+            Acumulador,
+            Caldera
+        }
+
+        public enum InstalacioCaldera
+        {
+            Cap,
+            Individual,
+            Centralitzada
+        }
+
+        public enum CalderaAntiga
+        {
+            Menys10Anys,
+            Mes10Anys
+        }
+
+        public enum TipusDescarrega
+        {
+            DobleDescarrega,
+            Senzilla
+        }
+
+        public enum CisternaEntrades
+        {
+            SenseEntrades,
+            UnaEntrada,
+            DobleEntrada
+        }
+    }
+}
